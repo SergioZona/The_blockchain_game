bf31f7f9af5b995b676ecc87cdf097809b90c1f7
SergioZona <60414505+SergioZona@users.noreply.github.com>
2023-02-20 22:13:14 -0500

    Primera pantalla implementada

--- blockchain-game-app/src/App.jsx
+++ blockchain-game-app/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Button, Grid, Container } from "@mui/material";
+
+import StartHost from "./components/host/StartHost";
+import StartGuest from "./components/guest/StartGuest";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Container className="App">
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <StartHost></StartHost>
+        </Grid>
+        <Grid item xs={6}>
+          <StartGuest></StartGuest>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export default App
+export default App;
---
+++ blockchain-game-app/src/components/host/StartHost.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+//import { Button, Container } from "react-bootstrap";
+import { Button } from "@mui/material";
+
+function Start() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      style={{ width: "25vh", height: "10vh" }}
+    >
+      Host a new game
+    </Button>
+  );
+}
+
+export default Start;
